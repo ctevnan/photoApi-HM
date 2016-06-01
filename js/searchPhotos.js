5effae0565138b9de528596b0eba3a7f08552caf
ctevnan <carolyntevnan@yahoo.com>
2016-06-01 11:09:36 -0400

    adding api endpoint number one

--- js/searchPhotos.js
+++ js/searchPhotos.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $(".btn-default").on("click", function(e){
     e.preventDefault();
     var userAddress = $("#userAddress").val();
+    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+    googleApiUrl += ""
 
     alert(userAddress);
   });  
