4f7e712f9c18cdd8fb7d1f0d77e6f389ba02ffb6
ctevnan <carolyntevnan@yahoo.com>
2016-06-02 12:41:54 -0400

    photos appending

--- js/searchPhotos.js
+++ js/searchPhotos.js
@@ -29,31 +29,34 @@ $(document).ready(function() {
           success: function(response) {
             var localPhotos = response.photos.photo;
             for(var i = 0; i < localPhotos.length; i++) {
-              buildThumbnail(localPhotos[i]);
+              var newCol = buildThumbnail(localPhotos[i]);
+              $("#photosRow").append(newCol);
             }
           }
         });
       }
     }); 
   });  
-});
-function buildThumbnail(photoData) {
-  var photoUrl = "https://farm" + photoData.farm;
-  photoUrl += ".staticflickr.com/" + photoData.server;
-  photoUrl += "/" + photoData.id;
-  photoUrl += "_" + photoData.secret + ".jpg";
 
-  var colDiv = $("<div>").addClass("col-md-3");
-  var thumbnailDiv = $("<div>").addClass("thumbnail");
-  var photoImg = $("<img>").attr("src", photoUrl);
-  var captionDiv = $("<div>").addClass("caption");
-  var picTitle = $("<h3>").append("photoData.title");
+  function buildThumbnail(photoData) {
+    var photoUrl = "https://farm" + photoData.farm;
+    photoUrl += ".staticflickr.com/" + photoData.server;
+    photoUrl += "/" + photoData.id;
+    photoUrl += "_" + photoData.secret + ".jpg";
 
-  colDiv.append(thumbnailDiv
-    .append(photoImg)
-    .append(captionDiv
-      .append(picTitle)
-    )
-  )
-  
+    var colDiv = $("<div>").addClass("col-md-3");
+    var thumbnailDiv = $("<div>").addClass("thumbnail");
+    var photoImg = $("<img>").attr("src", photoUrl);
+    var captionDiv = $("<div>").addClass("caption");
+    var picTitle = $("<h3>").append("photoData.title");
+
+    colDiv.append(thumbnailDiv
+      .append(photoImg)
+      .append(captionDiv
+        .append(picTitle)
+      )
+    );
     
+    return colDiv;
+  }
+});   
